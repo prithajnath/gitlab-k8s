4013254fb1a3ad39e23c0a7828e8a8f3b0caefcc
Prithaj Nath <prithajnath@gmail.com>
2019-02-27 21:59:38 +0000

    Add deployment commands in run script

--- run.sh
+++ run.sh
@@ -18,3 +18,17 @@ kubectl apply -f PVC/postgres-volume-claim.yml
 
 echo "Creating GitLab Persistent Volume Claim"
 kubectl apply -f PVC/gitlab-volume-claim.yml
+
+# Add some checks to verify PV and PVC creation
+# Create DB and cache
+
+echo "Creating Redis"
+kubectl apply -f Deployment/redis-deployment.yml
+
+echo "Creating Postgres"
+kubectl apply -f Deployment/postgres-deployment.yml
+
+# Add some checks to verify database and cache status
+# Create GitLab
+echo "Creating GitLab"
+kubectl apply -f Deployment/gitlab-deployment.yml
